bbaed27ef9efa1e7a2f7671e959db0969bb16f93
Greg Ells <gregells@gmail.com>
2025-04-17 15:22:03 -0400

    Add __str__ method to Cat model

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,8 @@ class Cat(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+    # Changing this instance method does not impact the database,
+    # therefore no migration is needed
+    def __str__(self):
+        return f'{self.name} ({self.id})'
