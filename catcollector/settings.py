1c30b877bd5976c9fcc023899152dbb1b02b67fa
Greg Ells <gregells@gmail.com>
2025-04-23 23:17:58 -0400

    Implement logout button in nav bar

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Specify the default redirect URL after successful login:
+# Specify the default redirect URL after successful login or logout:
 LOGIN_REDIRECT_URL = '/cats/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
